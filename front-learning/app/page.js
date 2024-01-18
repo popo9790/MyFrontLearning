9a9c884e1eb57ace81bf5d1a5b3e0789a00c22fc
朱佾軒 <sam1723257761@gmail.com>
2024-01-18 16:22:59 +0800

    Calculator FrontEnd

---
+++ front-learning/app/page.js
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <div class="Board">
+      <div class="Display"></div>
+      <div class="Numpad">
+        <div class="Numblock">1</div>
+        <div class="Numblock">2</div>
+        <div class="Numblock">3</div>
+        <div class="Numblock">+</div>
+        <div class="Numblock">4</div>
+        <div class="Numblock">5</div>
+        <div class="Numblock">6</div>
+        <div class="Numblock">-</div>
+        <div class="Numblock">7</div>
+        <div class="Numblock">8</div>
+        <div class="Numblock">9</div>
+        <div class="Numblock">*</div>
+        <div class="Numblock">0</div>
+        <div class="Equalblock">=</div>
+        <div class="Numblock">/</div>
+      </div>
+    </div>
+  );
+}
